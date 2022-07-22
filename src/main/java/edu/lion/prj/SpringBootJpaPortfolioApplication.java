2a75ccaa06ce179138cd0cc4bb37212592e16f04
TEACHER\PC-00 <nqwrt@naver.com>
2022-07-22 10:37:23 +0900

    first commit

---
+++ src/main/java/edu/lion/prj/SpringBootJpaPortfolioApplication.java
@@ -0,0 +1,13 @@
+package edu.lion.prj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJpaPortfolioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJpaPortfolioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/lion/prj/SpringBootJpaPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.lion.prj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJpaPortfolioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
